e14776f1da9c81c3c85c3cbe9e0647c6476d0693
watch_wu <2281126187@qq.com>
2024-05-16 18:59:08 +0800

    调整目录

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import '@/assets/main'
 
 const app = createApp(App)
 
