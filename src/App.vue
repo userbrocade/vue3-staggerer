634ff66dc550a812335fedbf308019abbec0cc95
watch_wu <2281126187@qq.com>
2024-05-20 17:37:42 +0800

    基于element-plus完成login静态布局

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
-<script setup>
-// 导入路由
-import { useRoute, useRouter } from 'vue-router'
-// 导入 pinia
-import { useUserStore } from '@/stores/pinia'
-// 获取路由对象
-const router = useRouter()
-// 获取路由参数
-const route = useRoute()
-
-const userstore = useUserStore()
-
-const skiplogin = () => {
-  console.log(router, route)
-  router.push('/login')
-}
-</script>
+<script setup></script>
 
 <template>
+  <!-- 一级路由出口 -->
   <router-view></router-view>
-  <hr />
-  <hr />
-  <hr />
-  <hr />
-  <el-button type="info" @click="$router.push('/Home')">点击跳转首页</el-button>
-  <el-button type="primary" @click="skiplogin">点击跳转登入页</el-button>
-  <div>{{ userstore.Token }}</div>
-  <el-button @click="userstore.setToken('faebrjibefijbos')">登入</el-button>
-  <el-button @click="userstore.removeToken()">退出</el-button>
 </template>
 
 <style scoped></style>
--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 
 <template>
+  <!-- 二级路由出口 -->
   <router-view></router-view>
   <div>布局架子</div>
 </template>
